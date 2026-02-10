839d6eadef529006c2e8ffe2f903d937d54ea5eb
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-10 22:50:37 +0100

    milestone 1: added articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
+const articles = ["The 'Dark Forest' Theory", "Deep Space Signals", "First Contact Protocol"];
+
 export default function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="container mt-5">
+      <h1 className="ps-2 mb-4">Blog Articles 🛸</h1>
+      <ul className="list-group fs-3 w-50">
+        {articles.map((article, index) => (
+          <li key={index} className="list-group-item">
+            {article}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
