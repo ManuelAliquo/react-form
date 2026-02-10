31ffbb28c98bfb8e2e085bf3535df7ebc5bda9fb
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-10 23:39:39 +0100

    milestone 2: added form

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ export default function App() {
   return (
     <div className="container mt-5">
       <h1 className="ps-2 mb-4">Blog Articles 🛸</h1>
-      <ul className="list-group fs-3 w-50">
-        {articles.map((article, index) => (
-          <li key={index} className="list-group-item">
-            {article}
-          </li>
-        ))}
-      </ul>
+      <div className="d-flex gap-5">
+        <ul className="list-group fs-3 w-50">
+          {articles.map((article, index) => (
+            <li key={index} className="list-group-item">
+              {article}
+            </li>
+          ))}
+        </ul>
+        <form className="fs-2">
+          <label htmlFor="new-article">New Article</label>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control fs-4" />
+            <button className="btn btn-primary fs-4" id="new-article">
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
